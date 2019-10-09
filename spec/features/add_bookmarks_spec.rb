0b5b6bd729ddea822777ba8ea5ad2c908660b6cf
Vijay Kurian <kurianvijay7@gmail.com>
2019-10-09 09:14:38 +0100

    Final commit before step 11

--- spec/features/add_bookmarks_spec.rb
+++ spec/features/add_bookmarks_spec.rb
@@ -1,4 +1,6 @@
-feature 'add bookmarks' do
+feature '.add bookmarks' do
+  # -> Why is this feature test  not adding anything to the test DB?
+  # -> Why are we not starting the test DB here again?
   scenario 'allows a user to add a bookmark' do
     visit('/bookmarks/new')
     fill_in :url, with: 'http://www.amazon.com'
